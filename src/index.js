91a225afdcd4923d6f6ba46ecc7797c1b9dc9853
exequiel381 <exequiel381@gmail.com>
2022-04-10 14:56:38 -0300

    video 5 terminado

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
-const express = require('express');
-const app = express();
+// Credenciales : https://www.mercadopago.com.ar/developers/panel/credentials
 
-          
-// SDK de Mercado Pago
+const express = require("express");
+const app = express();
 const mercadopago = require("mercadopago");
-// Agrega credenciales
-mercadopago.configure({
-  access_token: "PROD_ACCESS_TOKEN",
+const bodyParser = require("body-parser");
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.listen(3000, () => {
+  console.log("server running");
 });
 
-app.get('/checkout', (req,res) =>{
-    res.send('<h1>Hola desde checkout</h1>')
-})
+mercadopago.configure({
+  access_token:
+    "APP_USR-4186361912400271-072900-a4e561316429771f117f3d0b5b355e16-160737866",
+});
 
-//server
+app.post("/checkout", (req, res) => {
+  let preference = {
+    items: [
+      {
+        id: "34",
+        title: req.body.title,
+        unit_price: parseInt(req.body.unit_price),
+        quantity: 1,
+      },
+    ],
+  };
 
-app.listen(3000, () => {
-    console.log("server running");
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      res.redirect(response.body.init_point);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
