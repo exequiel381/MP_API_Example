c348c731041426ac51b47d7eeaa05dae7473eb88
exequiel381 <exequiel381@gmail.com>
2022-04-11 09:06:41 -0300

    routes for one item or any items

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Bienvenido a la API Mercado Pago</h1>");
 });
 
-app.get("/success/:idItem", (req, res) => {
+app.get("/successItem/:idItem", (req, res) => {
   res.send("" + req.params.idItem);
 });
 
@@ -44,34 +44,56 @@ app.get("/pending", (req, res) => {
   res.send("pending");
 });
 
-app.post("/checkout", (req, res) => {
-  // PODEMOS HACER PARA UN SOLO ITEM, Y MANDAR SU ID AL SUCCESS , O RECIBIR UNA COLECCION PARA COBRAR , PERO NO SE COMO MANDAR LA LISTA DE ITEMS AL SUCCESS
-  var idItem = "5";
+app.post("/checkout/item", (req, res) => {
+  // Recibimos un solo item (atributos por params), Y MANDAR SU ID AL SUCCESS
+  var id = "5";
   let preference = {
-    //items: req.body.itemsBody,
-
-    // items: req.body.itemsBody.map((item) => {
-    //   return {
-    //     id: item.id,
-    //     title: item.title,
-    //     unit_price: parseInt(item.unit_price),
-    //     quantity: item.quantity,
-    //   };
-    // }),
-
     items: [
       {
-        id: "34", //req.body.idItem,
-        title: "nada", //req.body.title,
-        unit_price: parseInt("1"), //parseInt(req.body.unit_price),
-        quantity: 1, //req.body.quantity,
+        id: "34", //req.query.id;,
+        title: "nada", //req.query.title,
+        unit_price: parseInt("1"), //parseInt(req.query.unit_price),
+        quantity: 1, //req.query.quantity,
       },
     ],
     auto_return: "approved",
     back_urls: {
-      success: "http://localhost:3000/success" + idItem, //req.body.back_url.success + req.body.idItem,
-      failure: "http://localhost:3000/faileture",
-      pending: "http://localhost:3000/pending",
+      success: "http://localhost:3000/successItem" + id, //req.query.successURL + req.query.id,
+      failure: "http://localhost:3000/faileture", //req.query.failetureURL
+      pending: "http://localhost:3000/pending", //req.query.pendingURL
+    },
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      res.redirect(response.body.init_point);
+      //res.send(response.body.init_point); // si usamos otro front , podemos pedir la url , para redireccionarlo.
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.post("/checkout/itemsBody", (req, res) => {
+  // RECIBE UNA LISTA DE ITEMS, RECIBIR UNA COLECCION PARA COBRAR , PERO NO SE COMO MANDAR LA LISTA DE ITEMS AL SUCCESS
+  // Manejamos desde el FRONT lo que seria un descuento de stock
+  let preference = {
+    //items: req.body.itemsBody,
+    items: req.body.itemsBody.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        unit_price: parseInt(item.unit_price),
+        quantity: item.quantity,
+      };
+    }),
+
+    auto_return: "approved",
+    back_urls: {
+      success: req.body.back_url.success,
+      failure: req.body.back_url.failure,
+      pending: req.body.back_url.pending,
     },
   };
 
